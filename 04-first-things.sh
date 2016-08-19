e2a562ef1aded52e41b6f7420ed1c2c174bcb523
Christian Weilguny <netdesk@users.noreply.github.com>
2016-08-20 00:19:31 +0200

    [TASK] script for first things after first boot

---
+++ 04-first-things.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# settings
+USERNAME=insertusernamehere
+HOSTNAME=set.your.hostname
+TIMEZONE=Europe/Vienna
+KEYMAP=de-latin1
+LOCALE=en_US.UTF-8
+
+# create a user
+useradd -m -G users,wheel -s /bin/bash $USERNAME
+passwd $USERNAME
+
+# system setup
+timedatectl set-timezone $TIMEZONE
+hostnamectl set-hostname $HOSTNAME
+localectl set-keymap $KEYMAP
+localectl set-locale $LOCALE
