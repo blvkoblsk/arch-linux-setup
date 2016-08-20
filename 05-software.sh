220f087e0db8c56bd9e8ba5ce3775e207513f025
Christian Weilguny <netdesk@users.noreply.github.com>
2016-08-21 00:26:10 +0200

    [TASK] added terminals

--- 05-software.sh
+++ 05-software.sh
@@ -25,8 +25,8 @@ pacman -S xorg-server xorg-server-utils xf86-video-intel xf86-input-synaptics bl
 
 # desktop
 pacman -S openbox obconf oblogout lxappearance-obconf-gtk3 lxdm compton tint2 slock
-pacman -S deepin-icon-theme numix-themes
-pacman -S geany nodejs npm blueberry
+pacman -S deepin-icon-theme deepin-gtk-theme
+pacman -S geany thunar nodejs npm blueberry guake lxterminal
 yaourt -S --noconfirm lxdm-themes
 
 # geany themes
