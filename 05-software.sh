78870383b0d13bea783c99820a15eb81365d3268
Christian Weilguny <netdesk@users.noreply.github.com>
2016-08-21 23:37:55 +0200

    [TASK] exchanged deepin gtk theme with arc

--- 05-software.sh
+++ 05-software.sh
@@ -25,9 +25,8 @@ pacman -S xorg-server xorg-server-utils xf86-video-intel xf86-input-synaptics bl
 
 # desktop
 pacman -S openbox obconf oblogout lxappearance-obconf-gtk3 lightdm light-locker lightdm-gtk-greeter compton plank tint2 slock
-pacman -S deepin-gtk-theme
 pacman -S geany thunar ristretto evince file-roller nodejs npm blueberry guake lxterminal
-yaourt -S --noconfirm lxdm-themes franz brightside skippy-xd-git arc-icon-theme
+yaourt -S --noconfirm lxdm-themes franz brightside skippy-xd-git arc-icon-theme gtk-theme-arc-git
 
 # infinality
 sudo pacman-key -r 962DDE58
